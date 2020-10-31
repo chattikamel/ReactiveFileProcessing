4ce7da55c236176517bc7f6a3d248e3865980d4d
Kamel CHATTI <chattikamel@gmail.com>
2020-10-31 18:40:11 +0100

    file loading

--- src/test/java/com/myhome/reactiveFileProcessor/ReactiveFileProcessorApplicationTests.java
+++ src/test/java/com/myhome/reactiveFileProcessor/ReactiveFileProcessorApplicationTests.java
@@ -1,13 +1,31 @@
 package com.myhome.reactiveFileProcessor;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
+
+import java.io.IOException;
 
 @SpringBootTest
 class ReactiveFileProcessorApplicationTests {
+
+	@Value("classpath:com/myhome/reactiveFileProcessor/data.csv")
+	Resource dataFile;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void fileResourceLoadTest() throws IOException {
+		Assertions.assertNotNull(dataFile);
+		Assertions.assertTrue(dataFile.getFile().canRead());
+	}
+
+	@Test
+	void fileParsingTest() {
+
+	}
 }
