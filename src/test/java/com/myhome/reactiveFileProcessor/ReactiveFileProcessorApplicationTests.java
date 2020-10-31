cd518bb612df37882c5673e60e0738e29d26f903
Kamel CHATTI <chattikamel@gmail.com>
2020-10-31 19:18:35 +0100

    refacto 'parsing csv file' test

--- src/test/java/com/myhome/reactiveFileProcessor/ReactiveFileProcessorApplicationTests.java
+++ src/test/java/com/myhome/reactiveFileProcessor/ReactiveFileProcessorApplicationTests.java
@@ -10,50 +10,57 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
+import static java.util.Arrays.asList;
+import static org.apache.commons.lang3.tuple.ImmutablePair.of;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class ReactiveFileProcessorApplicationTests {
 
-	@Value("classpath:com/myhome/reactiveFileProcessor/data.csv")
-	Resource dataFile;
+    @Value("classpath:com/myhome/reactiveFileProcessor/data.csv")
+    Resource dataFile;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
-	@Test
-	void fileResourceLoadTest() throws IOException {
-		assertNotNull(dataFile);
-		assertTrue(dataFile.getFile().canRead());
-	}
+    @Test
+    void fileResourceLoadTest() throws IOException {
+        assertNotNull(dataFile);
+        assertTrue(dataFile.getFile().canRead());
+    }
 
-	@Test
-	void fileParsingTest() throws IOException {
+    @Test
+    void fileParsingTest() throws IOException {
 
-		FileParser fileParser = new FileParser(dataFile.getFile());
-		List<Person> persons = new CsvToBeanBuilder(new FileReader(dataFile.getFile()))
-				.withType(Person.class).build().parse();
+        List<Person> persons = new CsvToBeanBuilder(new FileReader(dataFile.getFile()))
+                .withType(Person.class).build().parse();
 
-		assertEquals(3, persons.size());
+        assertEquals(3, persons.size());
 
-		Person expectedFirstPerson = Person.builder()
-				.firstName("kamel")
-				.lastName("happy")
-				.age(15)
-				.build();
+        asList(of(Person.builder()
+                        .firstName("kamel")
+                        .lastName("happy")
+                        .age(15)
+                        .build(), 0),
+
+                of(Person.builder()
+                        .firstName("mario")
+                        .lastName("game")
+                        .age(25)
+                        .build(), 2),
+
+                of(Person.builder()
+                        .firstName("adam")
+                        .lastName("unhappy")
+                        .age(20)
+                        .build(), 1)
+
+        )
+                .forEach(p -> assertEquals(p.getLeft(), persons.get(p.getRight())));
 
 
-		assertEquals(expectedFirstPerson, persons.get(0));
-
-		Person expectedSecondPerson = Person.builder()
-				.firstName("adam")
-				.lastName("unhappy")
-				.age(20)
-				.build();
-
-		assertEquals(expectedSecondPerson, persons.get(1));
+    }
 
 
-	}
 }
